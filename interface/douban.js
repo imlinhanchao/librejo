0e2e9a4e2f2ae8d94718bed5962dff543c140cff
Hancel.Lin <imlinhanchao@gmail.com>
2019-01-23 23:02:29 +0800

    新增豆瓣图书接口。

---
+++ interface/douban.js
@@ -0,0 +1,62 @@
+const req = require('../lib/req');
+const App = require('./app');
+
+const __encoding__ = 'utf-8';
+const __domain__ = 'https://api.douban.com/v2/book';
+
+let __error__ = Object.assign({}, App.error);
+__error__.query = App.error.reg('查询失败');
+
+const QUERY = '查询';
+
+class Module extends App {
+    constructor() {
+        super();
+        this.okquery = function (data) {
+            return App.ok(QUERY, data);
+        };
+    }
+
+    get error() {
+        return __error__;
+    }
+
+    static get cache() {
+        return {
+        };
+    }
+
+    async id(id) {
+        try {
+            let rsp = await req.get(`${__domain__}/${id}`, __encoding__);
+            return this.okquery(JSON.parse(rsp.body));
+        } catch (err) {
+            if (err.isdefine) throw (err);
+            throw (this.error.network);
+        }
+    }
+
+    async isbn(isbn) {
+        try {
+            let rsp = await req.get(`${__domain__}/isbn/${isbn}`, __encoding__);
+            return this.okquery(JSON.parse(rsp.body));
+        } catch (err) {
+            if (err.isdefine) throw (err);
+            throw (this.error.network);
+        }
+    }
+
+    async query(name) {
+        try {
+            let rsp = await req.get(`${__domain__}/search?q=${name}`, __encoding__);
+            return this.okquery(JSON.parse(rsp.body));
+        } catch (err) {
+            if (err.isdefine) throw (err);
+            throw (this.error.network);
+        }
+    }
+}
+
+module.exports = Module;
+
+
