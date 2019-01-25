2d4c21c156bea1b575db14d2d0e20d2dde8413b1
hancel.lin <imlinhanchao@foxmail.com>
2019-01-25 16:47:27 +0800

    修正代码格式

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import modules from './modules'
-Vue.use(Vuex)
+import Vue from 'vue';
+import Vuex from 'vuex';
+import modules from './modules';
+
+Vue.use(Vuex);
+
 export default new Vuex.Store({
-  modules,
-  strict: process.env.NODE_ENV !== 'production'
-})
+    modules,
+    strict: process.env.NODE_ENV !== 'production'
+});
