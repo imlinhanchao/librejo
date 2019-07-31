9a698331dc6434dad193204fb30f82977be06b43
Hancel.Lin <imlinhanchao@gmail.com>
2019-07-31 23:14:01 +0800

    更新依赖库

--- model/db.js
+++ model/db.js
@@ -1,5 +1,42 @@
 const Sequelize = require('sequelize');
 const config = require('./config').db;
+const Op = Sequelize.Op;
+const operatorsAliases = {
+    $eq: Op.eq,
+    $ne: Op.ne,
+    $gte: Op.gte,
+    $gt: Op.gt,
+    $lte: Op.lte,
+    $lt: Op.lt,
+    $not: Op.not,
+    $in: Op.in,
+    $notIn: Op.notIn,
+    $is: Op.is,
+    $like: Op.like,
+    $notLike: Op.notLike,
+    $iLike: Op.iLike,
+    $notILike: Op.notILike,
+    $regexp: Op.regexp,
+    $notRegexp: Op.notRegexp,
+    $iRegexp: Op.iRegexp,
+    $notIRegexp: Op.notIRegexp,
+    $between: Op.between,
+    $notBetween: Op.notBetween,
+    $overlap: Op.overlap,
+    $contains: Op.contains,
+    $contained: Op.contained,
+    $adjacent: Op.adjacent,
+    $strictLeft: Op.strictLeft,
+    $strictRight: Op.strictRight,
+    $noExtendRight: Op.noExtendRight,
+    $noExtendLeft: Op.noExtendLeft,
+    $and: Op.and,
+    $or: Op.or,
+    $any: Op.any,
+    $all: Op.all,
+    $values: Op.values,
+    $col: Op.col
+};
 var sequelize = new Sequelize(config.database, config.user, config.password, {
     host: config.host,
     dialect: config.dialect,
@@ -9,7 +46,8 @@ var sequelize = new Sequelize(config.database, config.user, config.password, {
         max: 5,
         min: 0,
         idle: 10000
-    }
+    },
+    operatorsAliases
 });
 const ID_TYPE = Sequelize.UUID;
 function defineModel(name, attributes, defineAttr = {}) {
